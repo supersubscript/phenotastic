b57b293e8496801cc56e40a3005334cd579668ff
Henrik Åhl <henrikaahl@gmail.com>
2020-03-09 16:53:14 +0000

    use warnings

--- phenotastic/__init__.py
+++ phenotastic/__init__.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 
 """Top-level package for Phenotastic."""
+import warnings
+
+import os
+import sys
+import vtk
+import numpy as np
 
 __author__ = """Henrik Ahl"""
 __email__ = 'hpa22@cam.ac.uk'

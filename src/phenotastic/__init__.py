5e92b8198654693c704ed87fad96a7bec8543fb9
semantic-release <semantic-release>
2026-02-17 17:47:01 +0000

    build(release): update version to 0.3.0
    Automatically generated by python-semantic-release

--- src/phenotastic/__init__.py
+++ src/phenotastic/__init__.py
@@ -9,7 +9,7 @@ from dotenv import load_dotenv
 
 from phenotastic.utils.project import dev_root
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 
 if (dev_root() / ".env").exists():
     load_dotenv(dev_root() / ".env")
